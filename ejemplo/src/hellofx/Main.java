d04e496aa92a5a8518615267cb8e5bd1609525d3
Javier Eduardo <javiereduardo55xd@gmail.com>
2022-08-18 13:11:22 -0700

    solucionando el bug de formato y BD

--- ejemplo/src/hellofx/Main.java
+++ ejemplo/src/hellofx/Main.java
@@ -1,27 +1,29 @@
 package hellofx;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        try {
+public class Main extends Application
+{
+    @Override public void start(Stage primaryStage) throws Exception
+    {
+        try
+        {
             Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
             Scene scene = new Scene(root, 600, 600);
             scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
             primaryStage.setTitle("Mi apps");
             primaryStage.setScene(scene);
             primaryStage.show();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e);
         }
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
