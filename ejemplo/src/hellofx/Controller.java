721fd72b766eef82c773a4a42bbdb5dfd0f7880e
Javier Eduardo <javiereduardo55xd@gmail.com>
2022-08-18 13:49:16 -0700

    e

--- ejemplo/src/hellofx/Controller.java
+++ ejemplo/src/hellofx/Controller.java
@@ -3,14 +3,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import com.mysql.cj.exceptions.ExceptionFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 public class Controller
 {
     @FXML private Label label1;
     private Button boton1;
+    private TextField input1;
+    private TextField input2;
+    private TextField input3;
     public void initialize()
     {
         // String javaVersion = System.getProperty("java.version");
@@ -22,15 +27,15 @@ public class Controller
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplo", "root",
-                "javi54321");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplo", "root", "javi54321");
             Statement stmt = con.createStatement();
+            String nombre = input1.getText();
             ResultSet rs = stmt.executeQuery("select * from tabla1;");
-            int rs1 = stmt.executeUpdate("INSERT INTO tabla1 (nombre, edad) VALUES ('nnuejnrsdu', 562)");
+            int rs1 = stmt.executeUpdate("INSERT INTO tabla1 (nombre, edad) VALUES ('" + nombre + "', 562)");
         }
         catch (Exception e)
         {
-            System.out.println("\n" + e);
+            System.err.println(e);
         }
     }
 }
